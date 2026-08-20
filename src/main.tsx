7cbe433e654fe8f58770bc62e70b2d34e2205b23
rogeryuan1981-rgb <rogeryuan1981@gmail.com>
2026-08-20 18:24:40 +0800

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "../app/globals.css";
+import "../app/employee-extra.css";
 import "../app/timeline.css";
 import "../app/admin/admin.css";
 import "../app/admin/status.css";
