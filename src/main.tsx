a65372e2fa362725ce7b0c1b9596621c526fb4a4
rogeryuan1981-rgb <rogeryuan1981@gmail.com>
2026-09-18 17:16:21 +0800

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "../app/admin/status.css";
 import "./supabase.css";
 import "./campaign-enhancements.css";
 import "./platform-layout.css";
+import "./purchase-discount.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
